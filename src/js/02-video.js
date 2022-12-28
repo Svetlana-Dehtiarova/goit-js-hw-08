01b95102c5a56bb6604524657701b961ebdf6b97
Svetlana-Dehtiarova <degsvet1977@gmail.com>
2022-12-28 18:18:22 +0200

    Task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  const stopTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  player.setCurrentTime(stopTime);
+}
