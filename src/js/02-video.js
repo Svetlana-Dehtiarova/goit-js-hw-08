c4b919b7eeece5614bc75858fecf52d6d9c13568
Svetlana-Dehtiarova <degsvet1977@gmail.com>
2022-12-29 20:24:18 +0200

    Task03 and update task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,15 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = function (event) {
+function onPlay(event) {
   localStorage.setItem('videoplayer-current-time', event.seconds);
-};
+}
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 if (localStorage.getItem('videoplayer-current-time')) {
-  const stopTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  const stopTime = JSON.parse(
+    localStorage.getItem('videoplayer-current-time') || 0
+  );
   player.setCurrentTime(stopTime);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+
+const contactFormEl = document.querySelector('.feedback-form');
+const inputEmail = document.querySelector('.feedback-form input');
+const inputMessage = document.querySelector('.feedback-form textarea');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+contactFormEl.addEventListener('input', throttle(onContactFormFieldInput, 500));
+contactFormEl.addEventListener('submit', onContactFormSubmit);
+
+function onContactFormFieldInput() {
+    const userInfoFromLS = JSON.stringify({ email: inputEmail.value, massage: inputMessage.value });
+    localStorage.setItem(STORAGE_KEY, userInfoFromLS);
+}
+  
+function onContactFormSubmit(event) {
+    event.preventDefault();
+    if (!event.target.email.value || !event.target.message.value) {
+        return alert('Please fill all fields');
+    }
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  contactFormEl.reset();
+  localStorage.removeItem('STORAGE_KEY');
+};
+
+fillContactFormFields();
+
+function fillContactFormFields(event) {
+    const outputInfo = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (outputInfo) {
+        inputEmail.value = outputInfo.email;
+        inputMessage.value = outputInfo.massage;
+    }
+};
+
 
